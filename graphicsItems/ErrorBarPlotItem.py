fb9e183091b10bfcc89ef27164ce7bd1fb890f0e
Ingo Breßler <dev@ingobressler.net>
2012-06-11 15:53:15 +0200

    cleanup of ErrorBarPlotItem.py

--- graphicsItems/ErrorBarPlotItem.py
+++ graphicsItems/ErrorBarPlotItem.py
@@ -1,32 +1,25 @@
-from pyqtgraph.Qt import QtGui, QtCore
-from pyqtgraph.Point import Point
-import pyqtgraph.functions as fn
-from .GraphicsItem import GraphicsItem
-from .GraphicsObject import GraphicsObject
-import numpy as np
-import scipy.stats
-import weakref
-import pyqtgraph.debug as debug
+from pyqtgraph.Qt import QtGui
 from ScatterPlotItem import ScatterPlotItem, PathSpotItem, Symbols, coords, makeSymbolPixmap
-from collections import OrderedDict
 
 __all__ = ['ErrorBarPlotItem']
 
 ## Build all symbol paths
-Symbols['b'] = QtGui.QPainterPath()
-coords['b'] = [ # bar, like H rotated
+KEY = 'b'
+Symbols[KEY] = QtGui.QPainterPath()
+coords[KEY] = [ # bar, like H rotated
         (-0.5, -0.5), (0., -0.5), (0., 0.5), (-0.5, 0.5),
         (0.5, 0.5), (0., 0.5), (0., -0.5), (0.5, -0.5) ]
 
-for k, c in coords.items():
-    Symbols[k].moveTo(*c[0])
-    for x,y in c[1:]:
-        Symbols[k].lineTo(x, y)
-    Symbols[k].closeSubpath()
+k, c = KEY, coords[KEY]
+Symbols[k].moveTo(*c[0])
+for x,y in c[1:]:
+    Symbols[k].lineTo(x, y)
+Symbols[k].closeSubpath()
 
 class ErrorBarPlotItem(ScatterPlotItem):
     
     def generateSpotItems(self):
+        """use a SpotItem which scales only in y individually"""
         for rec in self.data:
             if rec['item'] is None:
                 rec['item'] = BarSpotItem(rec, self)
@@ -34,6 +27,7 @@ class ErrorBarPlotItem(ScatterPlotItem):
         self.sigPlotChanged.emit(self)
 
 class BarSpotItem(PathSpotItem):
+    """Scales in y direction only"""
     def __init__(self, *args, **kwargs):
         PathSpotItem.__init__(self, *args, **kwargs)
 
